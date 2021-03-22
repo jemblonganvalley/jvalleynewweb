c52684efc1f0abe87341b7d14733427a30a6df03
jemblonganvalley <jemblonganvalley@gmail.com>
2021-03-22 14:53:08 +0700

    update

--- routes/index_route.js
+++ routes/index_route.js
@@ -2,7 +2,12 @@ const express = require('express')
 const index = express.Router()
 
 index.get('/', (req,res)=>{
-    res.render('index')
+    res.render('index', {
+        title : "Jvalley Home",
+        meta_desc : "Kursus Pemograman Komputer Gratis",
+        meta_title : "Jvalley Kursus IT Gratis",
+        meta_image : "https://picsum.photos/seed/fadliselaz/200"
+    })
 })
 
 module.exports = index
